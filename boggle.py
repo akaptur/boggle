49739a0dbc51314547d9f474af97045d205a0bfd
Allison Kaptur <allison.kaptur@gmail.com>
2013-02-28 23:49:07 -0500

    Attempts at optimizations (not successful) with @thomasballinger

--- boggle.py
+++ boggle.py
@@ -29,23 +29,24 @@ def create_dictionary(dictionary=dictionary_path):
     """
     try:
         with open(dictionary, 'r') as f:
-            return {line.strip().lower() for line in f.readlines()}
+            return [line.strip().lower() for line in f.readlines()]
     except IOError:
         print "No dictionary at", dictionary_path, ". Please enter path to dictionary at command line."
         sys.exit()
+
 
 def dfs(visited_nodes, graph, node=('',(None,None))):
     """
     Performs depth-first-search on the graph, returns True if the word is present
     """
-    global dictionary
+    global dictionary_set
     visited_nodes = visited_nodes + [node]
 
     word_fragment = "".join([letter for letter, position in visited_nodes]) 
     # print word_fragment
     # pdb.set_trace()
 
-    if len(word_fragment) >= 3 and word_fragment in dictionary:
+    if len(word_fragment) >= 3 and word_fragment in dictionary_set:
         yield word_fragment
 
     good_neighbors = [n for n in graph[node] if n not in visited_nodes]
@@ -54,9 +55,37 @@ def dfs(visited_nodes, graph, node=('',(None,None))):
         for result in dfs(visited_nodes, graph, neighbor):
             yield result
 
+# def is_in(word, dictionary):
+#     # print dictionary
+#     if not dictionary:
+#         return False
+#     if len(dictionary) == 1:
+#         if dictionary[0].startswith(word):
+#             return True
+#         else:
+#             return False
+#     length = len(dictionary)
+#     middle_element = dictionary[length/2]
+#     if middle_element > word:
+#         if middle_element.startswith(word):
+#             return True
+#         else:
+#             return is_in(word, dictionary[:length/2])
+#     else:
+#         return is_in(word, dictionary[length/2:])
+
+# def test_is_in():
+#     sys.setrecursionlimit(10)
+#     word = 'ch'
+#     dictionary = ['apples', 'chad', 'dog', 'lemur', 'paper', 'pragmatic', 'what']
+#     print is_in(word, dictionary)
+
 
 if __name__ == "__main__":
-    dictionary = create_dictionary()
+    dictionary_list = create_dictionary()
+    # test_is_in()
+    # pdb.set_trace()
+    dictionary_set = set(dictionary_list)
     print "dictionary created"
     board, position = boggle_graph.make_board(letters, n)
     graph = boggle_graph.make_graph(board, position)
